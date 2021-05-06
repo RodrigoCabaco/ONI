4ce59bbe92a93e761585d563aa35ffd6d00dcc96
Rodrigo Cabaço <42916568+RodrigoCabaco@users.noreply.github.com>
2021-05-06 16:25:53 +0100

    Updaet

--- cartas.py
+++ cartas.py
@@ -20,5 +20,29 @@ def part_1():
 				K+=(X/2)
 	print(int(K))
 
+def part_2():
+	P = input().split(' ')
+	N = int(P[0])
+	X = int(P[1] )
+	A = list(map(int,input().split(' ')))
+	B = list(map(int,input().split(' ')))
+	final = A
+	for i in A:
+		for l in B:
+			if i==l:
+				final.append(i)
+			elif i<l:
+				while i<l:
+					g = final[A.index(i)]
+					g+=X
+					if g == i:
+						final[A.index(i)]
+					elif g > i:
+						return [-1]
+						break
+	return(final)
+
+
 t = input()
 if t=="1":part_1()
+if t=="2":print(part_2())
