4701a6a8e8da6649d496c65fb55b603ee148d49a
Rodrigo Cabaço <42916568+RodrigoCabaco@users.noreply.github.com>
2021-05-06 10:43:56 +0100

    Update

--- cartas.py
+++ cartas.py
@@ -3,12 +3,15 @@ def part_1():
 	P = input().split(' ')
 	N = int(P[0])
 	X = int(P[1])
-	A = [1,4,6,2]
-	B = [1,5,6,5]
+	A = list(map(int,input().split(' ')))
+	B = list(map(int,input().split (' ')))
 	#Output = multiplos de 3 (1,5)(1,5)(4,5)(6,6)(2,1)
-	K = []
+	K = 0
 	for i in A:
 		for l in B:
 			if (i+l)%X==0:
-				K.append((i+l))
-	print(len(K))
+				K+=1
+	print(K)
+
+t = input()
+if t=="1":part_1()
